4f91b17ba9891fd48f68f39c36e9aa0eaa9e07c6
Anderson Lomba <andersonlomba172@outlook.com>
2022-10-12 16:28:18 -0300

    Todo List react  pronto

--- src/App - Copy.tsx
+++ src/App - Copy.tsx
@@ -6,8 +6,8 @@ import { AddArea } from './components/AddArea';
 
 const App = () => {
   const [list, setList] = useState<Item[]>([
-    { id: 1, name: 'Almoçar no Shopping', done: false },
-    { id: 2, name: 'Almoçar no Shopping', done: true },
+    { id: 1, name: 'Comprar o pão na padaria', done: false },
+    { id: 2, name: 'Comprar um bolo na padaria', done: true },
   ]);
 
   const handleAddTask = (taskName: string) => {
