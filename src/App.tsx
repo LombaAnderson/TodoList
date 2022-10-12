03ad52169248352c368324aac2707e3d853d8eb6
Anderson Lomba <andersonlomba172@outlook.com>
2022-10-12 16:31:59 -0300

    Todo List React  pronto

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const App = () => {
     setList(newList);
   }
 
-  // Função feita para tarefinha de casa.
+  // Função feita para tarefa.
   const handleTaskChange = (id: number, done: boolean) => {
     let newList = [...list];
     for(let i in newList) {
